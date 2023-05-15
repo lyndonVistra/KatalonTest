3f9c17311a3e3c452e4590c002b4bb333cd058ee
ldsouza <dsz.lyndon85@gmail.com>
2023-05-15 02:34:57 -0400

    Deleted a few cases Added OR and test case

--- Scripts/osc_login/Script1684130650347.groovy
+++ Scripts/osc_login/Script1684130650347.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('https://docs.katalon.com/docs/')
+WebUI.openBrowser(findTestData('OsC/osc_url_csv').getValue(1, 1))
 
 WebUI.delay(10)
 
